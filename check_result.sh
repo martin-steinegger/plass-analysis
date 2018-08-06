9f7a4254ff8f085b12b189040f85d6b81ceb9bf7
Martin Steinegger <martin.steinegger@mpibpc.mpg.de>
2018-08-07 01:42:29 +0200

    check_results script

---
+++ check_result.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+REPORT=$1
+VALS=$2
+if [[ ! -s "${REPORT}" ]]; then
+    exit 1
+fi
+
+GOOD=$(awk -v check="$VALS" 'BEGIN{split(check,checklist," ")}{if($3 == checklist[NR]){cnt=cnt+1}}END{if(cnt==NR){print "GOOD"}else{print "BAD"}}' "${REPORT}") 
+ERROR=0
+if [[ "$GOOD" != "GOOD" ]]; then
+    ERROR=$((ERROR+1))
+    >&2 echo "Failed check! Input: ${values[3]} Expected: $TARGET Comparison: ${TEST}"
+    continue
+fi
+
+if [ $ERROR -ne 0 ]; then
+    exit 1
+fi
+
+exit 0
