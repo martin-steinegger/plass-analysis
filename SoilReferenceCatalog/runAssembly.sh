82386025c2cdaaa4490e8e3ec0df3a580e60bacc
Martin Steinegger <martin.steinegger@mpibpc.mpg.de>
2018-08-06 18:05:43 +0200

    SRC script

---
+++ SoilReferenceCatalog/runAssembly.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+#BSUB -J download[1-640]
+#BSUB -o out.%I.%J
+#BSUB -e err.%I.%J
+
+id=$(sed "${LSB_JOBINDEX}q;d" soillist_src)
+mkdir -p $id
+cd $id
+if [  ! -f ${id}_aa ]
+then
+  if [ ! -f allreads ]; then
+    wget https://sra-download.ncbi.nlm.nih.gov/srapub/${id}
+    fastq-dump --split-files -split-3 ./$id
+  fi
+  mmseqs assemble ${id}_1.fastq ${id}_2.fastq ${id}_assembly.fas  /tmp/tmp_${id}
+  rm -rf /tmp/tmp_${id}
+  # clean up
+fi
