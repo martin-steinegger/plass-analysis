8e29cf0d997d666ec1532b30bd8b265ed4a448fa
Martin Steinegger <martin.steinegger@mpibpc.mpg.de>
2018-08-07 03:04:00 +0200

    codeship error fix

--- run_codeship_pipeline.sh
+++ run_codeship_pipeline.sh
@@ -32,15 +32,16 @@ export PATH=$MMSEQSPATH:$PATH
 #setup benchmark database
 mkdir results
 
-# go run it
 PLASSANALPATH="$(realpath plass-analysis)/Prochloroccus/"
-
 export PATH=$PLASSANALPATH:$PATH
 
+# go run it
 plass assemble ./plass-analysis/data/allgenomes_reads_sample_1.fastq ./plass-analysis/data/allgenomes_reads_sample_2.fastq results/final.contigs.aa.fa results/tmp
 mmseqs createdb results/final.contigs.aa.fa results/final.contigs.aa
 chmod -R u+x ./plass-analysis/
+export PATH="$(realpath plass-analysis)":$PATH
 
+# eval it
 mmseqs createdb ./plass-analysis/data/prochloroccus_allproteins.fasta ./plass-analysis/data/prochloroccus_allproteins
 mmseqs createdb ./plass-analysis/data/prochloroccus_allproteins_nr.fasta ./plass-analysis/data/prochloroccus_allproteins_nr
 evaluateResults.sh results/final.contigs.aa ./plass-analysis/data/prochloroccus_allproteins ./plass-analysis/data/prochloroccus_allproteins_nr results/ 100 > log-${CI_COMMIT_ID}
@@ -51,7 +52,7 @@ cat results/precision >> report-${CI_COMMIT_ID}
 # fill out the report and fail
 cat log-${CI_COMMIT_ID}
 cat report-${CI_COMMIT_ID}
-./check_results.sh report-${CI_COMMIT_ID} "0.491 0.469 0.447 0.419 0.387 0.343 0.298 0.251 0.202 0.141 0.977 0.976 0.976 0.976 0.975 0.973 0.967 0.947 0.886 0.679"
+check_results.sh report-${CI_COMMIT_ID} "0.491 0.469 0.447 0.419 0.387 0.343 0.298 0.251 0.202 0.141 0.977 0.976 0.976 0.976 0.975 0.973 0.967 0.947 0.886 0.679"
 
 exit $?
 
