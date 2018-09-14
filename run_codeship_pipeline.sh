7f4abe1b8022e3f5c8c312934a7cfc2f6454301c
Martin Steinegger <martin.steinegger@mpibpc.mpg.de>
2018-09-14 16:09:17 +0200

    Remove wrong cats

--- run_codeship_pipeline.sh
+++ run_codeship_pipeline.sh
@@ -50,8 +50,6 @@ cat results/sense > report-${CI_COMMIT_ID}
 cat results/precision >> report-${CI_COMMIT_ID}
 
 # fill out the report and fail
-cat log-${CI_COMMIT_ID}
-cat report-${CI_COMMIT_ID}
 check_result.sh report-${CI_COMMIT_ID} "0.491 0.469 0.447 0.419 0.387 0.343 0.298 0.251 0.202 0.141 0.977 0.976 0.976 0.976 0.975 0.973 0.967 0.947 0.886 0.679"
 
 exit $?
