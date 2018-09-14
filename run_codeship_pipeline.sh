596f2fe72b2267bc7f238488b9d233c69a1248d8
Martin Steinegger <martin.steinegger@mpibpc.mpg.de>
2018-09-14 17:35:58 +0200

    New validation values

--- run_codeship_pipeline.sh
+++ run_codeship_pipeline.sh
@@ -50,7 +50,6 @@ cat results/sense > report-${CI_COMMIT_ID}
 cat results/precision >> report-${CI_COMMIT_ID}
 cat report-${CI_COMMIT_ID}
 # fill out the report and fail
-check_result.sh report-${CI_COMMIT_ID} "0.491 0.469 0.447 0.419 0.387 0.343 0.298 0.251 0.202 0.141 0.977 0.976 0.976 0.976 0.975 0.973 0.967 0.947 0.886 0.679"
-
+check_result.sh report-${CI_COMMIT_ID} "0.495 0.475 0.451 0.422 0.389 0.344 0.296 0.245 0.197 0.135 0.979 0.978 0.978 0.978 0.976 0.974 0.965 0.940 0.872 0.663"
 exit $?
 
